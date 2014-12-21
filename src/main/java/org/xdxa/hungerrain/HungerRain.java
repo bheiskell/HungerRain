77cb213dee4f07c791fc0428c8fd2000f4edfd69
Ben Heiskell <ben.heiskell@gmail.com>
2014-12-20 23:04:03 -0500

    feat(armor): add damage prevention by using full leather armor set
    When wearing a full leather armor set, HungerRain's food damage is prevented. Instead, the damage is dealt to the armor at a lower (configurable) frequency.

--- src/main/java/org/xdxa/hungerrain/HungerRain.java
+++ src/main/java/org/xdxa/hungerrain/HungerRain.java
@@ -22,18 +22,21 @@ import com.google.common.collect.Sets;
  */
 public class HungerRain extends JavaPlugin {
     private static final String COMMENT =
-"HungerRain has two primary settings:\n" +
-"  frequency: the number of server ticks per-execution of each of the hunger rules\n" +
-"  depletion-rate: rate at which hunger will deplete per execution of the rules\n\n" +
+"HungerRain has the following settings:\n" +
+"  frequency:              the number of server ticks per-execution of each of the hunger rules\n" +
+"  armor-damage-frequency: number of times a leather armor set can prevent damage before its durability decreases\n" +
+"  *-depletion-rate:       rate at which hunger will deplete per execution of the rules\n\n" +
 "Additionally, the snow rule allows the player to travel within a certain light threshold to stay warm.";
 
-    private static final String KEY_FREQUENCY = "frequency";
-    private static final String KEY_RAIN_DEPLETION_RATE = "rain-depletion-rate";
-    private static final String KEY_WATER_DEPLETION_RATE = "water-depletion-rate";
-    private static final String KEY_SNOW_DEPLETION_RATE = "snow-depletion-rate";
+    private static final String KEY_FREQUENCY                 = "frequency";
+    private static final String KEY_ARMOR_DAMAGE_FREQUENCY    = "armor-damage-frequency";
+    private static final String KEY_RAIN_DEPLETION_RATE       = "rain-depletion-rate";
+    private static final String KEY_WATER_DEPLETION_RATE      = "water-depletion-rate";
+    private static final String KEY_SNOW_DEPLETION_RATE       = "snow-depletion-rate";
     private static final String KEY_SNOW_REQUIRED_LIGHT_LEVEL = "snow-required-light-level";
 
-    private static final long DEFAULT_FREQUENCY = 20;
+    private static final long DEFAULT_FREQUENCY              = 100; // 5 seconds
+    private static final long DEFAULT_ARMOR_DAMAGE_FREQUENCY = 5;   // 100 * 5 = 25 seconds
 
     private static final Set<String> debuggers = Sets.newHashSet();
 
@@ -55,16 +58,18 @@ public class HungerRain extends JavaPlugin {
         config.options().copyDefaults(true);
 
         config.addDefault(KEY_FREQUENCY,                 DEFAULT_FREQUENCY);
+        config.addDefault(KEY_ARMOR_DAMAGE_FREQUENCY,    DEFAULT_ARMOR_DAMAGE_FREQUENCY);
         config.addDefault(KEY_RAIN_DEPLETION_RATE,       RainEnvironmentHungerStrategy.DEFAULT_DEPLETION_RATE);
         config.addDefault(KEY_WATER_DEPLETION_RATE,      WaterEnvironmentHungerStrategy.DEFAULT_DEPLETION_RATE);
         config.addDefault(KEY_SNOW_DEPLETION_RATE,       SnowEnvironmentHungerStrategy.DEFAULT_DEPLETION_RATE);
         config.addDefault(KEY_SNOW_REQUIRED_LIGHT_LEVEL, SnowEnvironmentHungerStrategy.DEFAULT_REQUIRED_LIGHT_LEVEL);
 
-        final long delay             = config.getInt(KEY_FREQUENCY);
-        final int rainDepletionRate  = config.getInt(KEY_RAIN_DEPLETION_RATE);
-        final int waterDepletionRate = config.getInt(KEY_WATER_DEPLETION_RATE);
-        final int snowDepletionRate  = config.getInt(KEY_SNOW_DEPLETION_RATE);
-        final int snowRequiredLight  = config.getInt(KEY_SNOW_REQUIRED_LIGHT_LEVEL);
+        final long delay                = config.getInt(KEY_FREQUENCY);
+        final long armorDamageFrequency = config.getInt(KEY_ARMOR_DAMAGE_FREQUENCY);
+        final int rainDepletionRate     = config.getInt(KEY_RAIN_DEPLETION_RATE);
+        final int waterDepletionRate    = config.getInt(KEY_WATER_DEPLETION_RATE);
+        final int snowDepletionRate     = config.getInt(KEY_SNOW_DEPLETION_RATE);
+        final int snowRequiredLight     = config.getInt(KEY_SNOW_REQUIRED_LIGHT_LEVEL);
 
         saveConfig();
 
@@ -79,6 +84,7 @@ public class HungerRain extends JavaPlugin {
         final HungerRainTask hungerRainTask = new HungerRainTask(
                 this.getServer(),
                 strategies,
+                armorDamageFrequency,
                 debuggers,
                 environmentContextFactory);
         hungerRainTask.runTaskTimer(this, delay, delay);
--- src/main/java/org/xdxa/hungerrain/HungerRainTask.java
+++ src/main/java/org/xdxa/hungerrain/HungerRainTask.java
@@ -5,12 +5,17 @@ import java.util.Set;
 import java.util.logging.Logger;
 
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.xdxa.hungerrain.environment.EnvironmentContext;
 import org.xdxa.hungerrain.environment.EnvironmentContextFactory;
 import org.xdxa.hungerrain.strategies.IEnvironmentHungerStrategy;
+
+import com.google.common.collect.Lists;
 
 /**
  * Task which executes configured {@link IEnvironmentHungerStrategy}s on each {@link Player}.
@@ -20,24 +25,37 @@ public class HungerRainTask extends BukkitRunnable {
     @SuppressWarnings("unused")
     private static final Logger LOG = Logger.getLogger("Minecraft.HungerRain");
 
+    private static final int TOTAL_ARMOR_SLOTS = 4;
+    private static final List<Material> LEATHER_ARMOR = Lists.newArrayList(
+            Material.LEATHER_BOOTS,
+            Material.LEATHER_CHESTPLATE,
+            Material.LEATHER_LEGGINGS,
+            Material.LEATHER_HELMET);
+
     private final Server server;
     private final List<IEnvironmentHungerStrategy> strategies;
+    private final long armorDamageFrequency;
     private final Set<String> debuggers;
     private final EnvironmentContextFactory environmentContextFactory;
+
+    private long armorIterations = 0;
 
     /**
      * Initialize the instance.
      * @param server the Bukkit server
      * @param strategies the strategies to execute
+     * @param armorDamageFrequency the number of times {@link #run()} is executed before an armor set takes damage
      * @param debuggers players which receieve debug output
      * @param environmentContextFactory the environment context factory
      */
     public HungerRainTask(final Server server,
                           final List<IEnvironmentHungerStrategy> strategies,
+                          final long armorDamageFrequency,
                           final Set<String> debuggers,
                           final EnvironmentContextFactory environmentContextFactory) {
         this.server = server;
         this.strategies = strategies;
+        this.armorDamageFrequency = armorDamageFrequency;
         this.debuggers = debuggers;
         this.environmentContextFactory = environmentContextFactory;
     }
@@ -51,22 +69,26 @@ public class HungerRainTask extends BukkitRunnable {
 
         for (final Player player : players) {
             final int initialFoodLevel = player.getFoodLevel();
-            int foodLevel = initialFoodLevel;
 
             if (GameMode.CREATIVE.equals(player.getGameMode())) {
                 sendMessage(player, "Skipping creative player");
                 continue;
             }
 
-            if (foodLevel == 0) {
+            if (initialFoodLevel == 0) {
                 sendMessage(player, "Skipping empty stomache");
                 continue;
             }
 
+            int foodDamage = 0;
             final EnvironmentContext environmentContext = environmentContextFactory.create(player.getLocation());
             for (final IEnvironmentHungerStrategy strategy : strategies) {
-                foodLevel -= strategy.evaluate(environmentContext);
+                foodDamage += strategy.evaluate(environmentContext);
             }
+
+            foodDamage = protectWithArmor(foodDamage, player);
+
+            int foodLevel = initialFoodLevel - foodDamage;
 
             sendMessage(player, environmentContext);
 
@@ -80,6 +102,61 @@ public class HungerRainTask extends BukkitRunnable {
 
             player.setFoodLevel(foodLevel);
         }
+    }
+
+    /**
+     * If the player is wearing leather armor, prevent the hunger damage and instead damage the armor.
+     * TODO: Find a better abstraction for this logic.
+     * @param foodDamage the food damage to be delivered
+     * @param player the player
+     * @return the food damage to be delivered (either the input or zero depending upon equipment)
+     */
+    private int protectWithArmor(final int foodDamage, final Player player) {
+        final EntityEquipment entityEquipment = player.getEquipment();
+        final ItemStack[] armorContents = entityEquipment.getArmorContents();
+
+        // We don't want to inflict armor damage every time.
+        armorIterations = (armorIterations + 1) % armorDamageFrequency;
+
+        int armorCount = 0;
+        for (final ItemStack armor : armorContents) {
+            if (LEATHER_ARMOR.contains(armor.getType())) {
+                armorCount++;
+            }
+        }
+
+        if (armorCount == TOTAL_ARMOR_SLOTS) {
+
+            if (armorIterations != 0) {
+                sendMessage(player, "Armor damage in ", armorDamageFrequency - armorIterations);
+                return 0;
+            }
+
+            sendMessage(player, "Armor prevented hunger damage, durability damage dealt: ", foodDamage);
+
+            for (int i = 0; i < armorContents.length; i++) { // need the index to remove destroyed armor
+
+                final ItemStack armor = armorContents[i];
+                if (LEATHER_ARMOR.contains(armor.getType())) {
+
+                    final short durability = (short)(armor.getDurability() + foodDamage);
+                    final short maxDurability = armor.getType().getMaxDurability();
+
+                    if (durability < maxDurability) {
+                        armor.setDurability(durability);
+                        sendMessage(player, armor.getType(), " durability: ", durability, " / ", maxDurability);
+                    } else {
+                        armorContents[i] = null;
+                    }
+                }
+            }
+            // armorContents is a copy, which needs to be re-set on the entityEquipment
+            entityEquipment.setArmorContents(armorContents);
+
+            return 0;
+        }
+
+        return foodDamage;
     }
 
     private void sendMessage(final Player player, final Object ... messages) {
